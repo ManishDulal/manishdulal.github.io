1bb9b74719ee95766ed666fc4a1c0ba9becf2d7f
ManishDulal <manishaphrodite1997@gmail.com>
2026-05-24 19:43:58 +0000

    Deploying to gh-pages from @ ManishDulal/manishdulal.github.io@f23e50b8094fefb4e4f4aee2a9b7b00f89c45fa9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-",
+          title: "",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
